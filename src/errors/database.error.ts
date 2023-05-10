fc2e42e8348c866893426842a27b345b5fc7cd90
romainm <romainm@MBP-de-Romain.lan>
2023-05-10 23:31:15 +0200

    [ADD] explicit typing

--- src/errors/database.error.ts
+++ src/errors/database.error.ts
@@ -1,13 +1,15 @@
 /**
  * Error raised when an error occurs with the database.
  *
+ * @property {string} name - Error name.
  */
 class DatabaseError extends Error {
+  name: string;
   /**
    *
    * @param {string} message - Error message.
    */
-  constructor(message = 'Database did not response.') {
+  constructor(message: string = 'Database did not response.') {
     super(message);
     this.name = 'DatabaseError';
   }
